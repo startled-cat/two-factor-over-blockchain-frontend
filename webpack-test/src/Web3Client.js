638f345a8e4f5955abf0f6959f4cd14f15c3f98d
Adam Kowalczyk <kombajno678@gmail.com>
2022-04-02 17:48:23 +0200

    can connect to MetaMask wallet

--- webpack-test/src/Web3Client.js
+++ webpack-test/src/Web3Client.js
@@ -1,54 +1,66 @@
 // import fetch from 'node-fetch';
 import Web3 from 'web3';
+import axios from "axios";
+
+
 const networks = require("./networks.json");
 
 const network_config = networks[networks.default];
 
-var web3 = new Web3(network_config.gateway_url);
+// var web3 = new Web3(network_config.gateway_url);
 
-// const DEFAULT_GAS_LIMIT = "2000000";
 
-// async function getCoinPrice(network_config) {
-//     return new Promise((resolve, reject) => {
-//         fetch(network_config.native_coin_price_api, { method: "get", headers: { 'Content-Type': 'application/json' } })
-//             .then((res) => res.json())
-//             .then((json) => {
-//                 resolve(json["USD"]);
-//             });
-//     })
-// }
 
-// async function getAbi(network_config) {
-//     let url = new URL(network_config.api_url);
-//     url.searchParams.set('module', 'contract');
-//     url.searchParams.set('action', 'getabi');
-//     url.searchParams.set('address', network_config.contract_address);
-//     url.searchParams.set('apikey', network_config.explorer_api_key);
-//     return new Promise((resolve, reject) => {
-//         fetch(url, { method: "get", headers: { 'Content-Type': 'application/json' } })
-//             .then((res) => res.json())
-//             .then((json) => {
-//                 resolve(JSON.parse(json["result"]));
-//             });
-//     })
-// }
 
-// async function getContract(network_config, account_address) {
-//     const abi = await getAbi(network_config);
-//     const contract = new web3.eth.Contract(abi, network_config.contract_address, {
-//         from: account_address, // default from address
-//     });
-//     return contract;
-// }
+const DEFAULT_GAS_LIMIT = "2000000";
 
-// async function makeContractCall(contracj_obj, method_name, params) {
-//     await contracj_obj.methods[method_name](...params).call({
-//         from: "",
-//         gas: DEFAULT_GAS_LIMIT
-//     }).then((result) => {
-//         console.log({ method_name, result });
-//     })
-// }
+async function getCoinPrice(network_config) {
+    return new Promise((resolve, reject) => {
+        axios.get(network_config.native_coin_price_api, {
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then((res) => res.data)
+            .then((json) => {
+                resolve(json["USD"]);
+            });
+    })
+}
+
+async function getAbi(network_config) {
+    return new Promise((resolve, reject) => {
+        axios.get(network_config.api_url, {
+            headers: { 'Content-Type': 'application/json' },
+            params: {
+                module: "contract",
+                action: "getabi",
+                address: network_config.contract_address,
+                apikey: network_config.explorer_api_key,
+            },
+        })
+            .then((res) => res.data)
+            .then((json) => {
+                resolve(JSON.parse(json["result"]));
+            });
+    })
+}
+
+async function getContract(network_config) {
+    const abi = await getAbi(network_config);
+    return new window.web3.eth.Contract(abi, network_config.contract_address);
+}
+
+async function makeContractCall(contracj_obj, method_name, params) {
+    return new Promise((resolve, reject) => {
+        contracj_obj.methods[method_name](...params).call({
+            from: "",
+            gas: DEFAULT_GAS_LIMIT
+        }).then((result) => {
+            console.log({ method_name, result });
+            resolve(result);
+        })
+    })
+
+}
 
 // async function makeContractTransaction(contracj_obj, method_name, params, account) {
 //     const methodCall = contracj_obj.methods[method_name](...params);
@@ -94,16 +106,13 @@ var web3 = new Web3(network_config.gateway_url);
 //     })
 // }
 
-// async function getGasPrice() {
-//     const networkId = await web3.eth.net.getId();
-//     console.log({ networkId });
-
-//     let gasPrice = await web3.eth.getGasPrice().then((result) => {
-//         console.log({ gasPriceInGwei: web3.utils.fromWei(result, 'gwei') });
-//         return result;
-//     });
-//     return gasPrice;
-// }
+async function getGasPrice() {
+    let gasPrice = await window.web3.eth.getGasPrice().then((result) => {
+        console.log({ gasPriceInGwei: window.web3.utils.fromWei(result, 'gwei') });
+        return result;
+    });
+    return gasPrice;
+}
 
 // export { web3, getCoinPrice, getAbi, getContract, makeContractCall, makeContractTransaction, getGasPrice }
-export { web3 }
+export { networks, getGasPrice, getContract, getCoinPrice, makeContractCall }
--- webpack-test/src/index.js
+++ webpack-test/src/index.js
@@ -1,16 +1,105 @@
-import { generator } from './Generator.js';
-import { web3 } from "./Web3Client.js"
+import Web3 from 'web3';
+import { web3, networks, getGasPrice, getContract, getCoinPrice, makeContractCall } from "./Web3Client.js"
 import "./styles/main.scss";
 
-const sth = async () => {
-    console.log("shits working looks like", generator());
-    const networkId = await web3.eth.net.getId();
-    console.log({ networkId });
+var contract;
 
-    document.getElementById("getNetworkIdButton").onclick = async () => {
-        const networkId = await web3.eth.net.getId();
-        console.log({ networkId });
-        document.getElementById("networkId").innerHTML = `Network id is ${networkId}`;
+const connectBtn = document.getElementById("connect");
+var currentNetworkId = null;
+
+
+
+
+const initialize = async () => {
+    let network_config;
+    if (currentNetworkId in networks) {
+        network_config = networks[currentNetworkId];
+    } else {
+        return;
+    }
+
+    console.log("INITIALIZE " + network_config.name);
+    document.getElementById("networkName").innerHTML = network_config.name;
+    document.getElementById("contractUrl").href = `${network_config.explorer_url}address/${network_config.contract_address}`;
+    document.getElementById("contractUrl").innerHTML = "Contract";
+    let gas = await getGasPrice();
+    document.getElementById("gasPrice").innerHTML = `gas price is ${window.web3.utils.fromWei(gas, "gwei")} gwei`;
+    document.getElementById("getOtpButton").onclick = async () => {
+        const otp = await makeContractCall(contract, "getNewOtp", []);
+        alert(`${otp}`);
     };
+    let coinPrice = await getCoinPrice(network_config);
+    contract = await getContract(network_config);
+
+    let methods_container = document.getElementById("methods");
+    methods_container.innerHTML = "";
+
+    Object.keys(contract.methods).filter(x => !x.includes("0x") && !x.includes("(")).forEach(method_name => {
+        let d = document.createElement("div");
+        d.innerHTML = method_name;
+        d.id = method_name;
+        d.class = "";
+        methods_container.appendChild(d);
+
+        let btn = document.createElement("button");
+        btn.setAttribute("class", "btn btn-outline-primary")
+        btn.innerHTML = "Call";
+        btn.onclick = () => makeContractCall(contract, method_name, []);
+        d.appendChild(btn);
+
+
+    })
+
 }
-sth();
+
+const setupEventListeners = async () => {
+    window.ethereum.on('accountsChanged', async function (accounts) {
+        console.log('accountsChanges', accounts);
+        initialize();
+
+    });
+
+    // detect Network account change
+    window.ethereum.on('networkChanged', async function (networkId) {
+        console.log('networkChanged', networkId);
+        currentNetworkId = networkId;
+        initialize();
+    });
+}
+
+
+
+const ethEnabled = async () => {
+    if (window.ethereum) {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        window.web3 = new Web3(window.ethereum);
+        currentNetworkId = window.ethereum.networkVersion;
+        console.log({ currentNetworkId });
+        connectBtn.disabled = true;
+        return true;
+    }
+    connectBtn.disabled = false;
+    return false;
+}
+
+const main = async () => {
+    setupEventListeners();
+
+
+
+    connectBtn.onclick = async () => {
+        let connected = await ethEnabled();
+        if (connected) {
+            initialize();
+        }
+    }
+    let network_names = Object.keys(networks).filter(x => !x.includes("default"));
+    console.log({ network_names });
+
+
+
+
+    // initialize(networks[networks.default]);
+}
+
+main();
